7e283ffbd3c1f309f122eed15c99804fe22f765b
Nestor Hasin <nestorhasin@gmail.com>
2022-06-26 14:36:35 -0300

    First commit

---
+++ src/test/java/com/configuration/configserverkafka/ConfigServerKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configuration.configserverkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
